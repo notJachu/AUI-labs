724673b39b19eb1dbc33f7a8397a1ab52eb23376
notJachu <jasiustasiu50@interia.pl>
2025-10-12 13:58:47 +0200

    init

---
+++ src/main/java/jachu/pg/auilabs/AuiLabsApplication.java
@@ -0,0 +1,13 @@
+package jachu.pg.auilabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuiLabsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuiLabsApplication.class, args);
+    }
+
+}
