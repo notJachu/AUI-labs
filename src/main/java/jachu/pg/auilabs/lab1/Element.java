471f421816895aa9188327ed05d164eeba28699d
notJachu <jasiustasiu50@interia.pl>
2025-10-12 15:09:02 +0200

    expanded Element with lombok

--- src/main/java/jachu/pg/auilabs/lab1/Element.java
+++ src/main/java/jachu/pg/auilabs/lab1/Element.java
@@ -1,8 +1,41 @@
 package jachu.pg.auilabs.lab1;
 
-public class Element {
-    String name;
-    int value;
 
-    Category category;
+import lombok.*;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Element implements Comparable<Element>{
+    private String name;
+    private int value;
+
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private Category category = null;
+
+    public void setCategory(Category category) {
+        if(this.category != null) {
+            this.category.getElemtns().remove(this);
+        }
+        this.category = category;
+        if(category != null && !category.getElemtns().contains(this)) {
+            category.getElemtns().add(this);
+        }
+    }
+
+    public ElementDto toDto() {
+        return ElementDto.builder()
+                .name(this.name)
+                .value(this.value)
+                .categoryName(this.category != null ? this.category.getName() : null)
+                .build();
+    }
+
+    @Override
+    public int compareTo(Element o) {
+        return this.name.compareTo(o.name);
+    }
 }
