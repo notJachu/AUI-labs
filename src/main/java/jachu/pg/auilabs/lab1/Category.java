0b16e16ed2b3efff5ceb520e171fab0e7963e2e8
notJachu <jasiustasiu50@interia.pl>
2025-10-12 15:04:22 +0200

    expanded category with lombok

--- src/main/java/jachu/pg/auilabs/lab1/Category.java
+++ src/main/java/jachu/pg/auilabs/lab1/Category.java
@@ -1,14 +1,24 @@
 package jachu.pg.auilabs.lab1;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class Category {
-    @Getter
-    @Setter
-    String name;
-    int someValue;
-    List<Element> elemtns;
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category implements Comparable<Category>{
+    private String name;
+    private int someValue;
+
+    @Builder.Default
+    @ToString.Exclude
+    private List<Element> elemtns = new ArrayList<>();
+
+    @Override
+    public int compareTo(Category o) {
+        return this.name.compareTo(o.name);
+    }
 }
