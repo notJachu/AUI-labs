51a4306269bde2dacc63418b637992907c384a73
notJachu <jasiustasiu50@interia.pl>
2025-10-21 20:52:03 +0200

    implemented repositories for elements and categories

--- src/main/java/jachu/pg/auilabs/repositories/CategoryRepository.java
+++ src/main/java/jachu/pg/auilabs/repositories/CategoryRepository.java
@@ -2,8 +2,14 @@ package jachu.pg.auilabs.repositories;
 
 import jachu.pg.auilabs.lab1.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, UUID> {
+    Optional<Category> findByName(String name);
+    Optional<Category> findByUuid(UUID uuid);
+
 }
--- src/main/java/jachu/pg/auilabs/repositories/ElementRepository.java
+++ src/main/java/jachu/pg/auilabs/repositories/ElementRepository.java
@@ -1,9 +1,16 @@
 package jachu.pg.auilabs.repositories;
 
+import jachu.pg.auilabs.lab1.Category;
 import jachu.pg.auilabs.lab1.WheelBarrow;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface ElementRepository extends JpaRepository<WheelBarrow, UUID> {
+    Optional<WheelBarrow> findByUuid(UUID uuid);
+    Optional<WheelBarrow> findByName(String name);
+    Optional<WheelBarrow> findByCategory(Category category);
 }
