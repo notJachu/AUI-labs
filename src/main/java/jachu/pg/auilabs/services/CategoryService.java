9881735e770c357381e181707bc1098fb0358cdf
notJachu <jasiustasiu50@interia.pl>
2025-10-21 21:03:17 +0200

    Category service implemented

---
+++ src/main/java/jachu/pg/auilabs/services/CategoryService.java
@@ -0,0 +1,41 @@
+package jachu.pg.auilabs.services;
+
+import jachu.pg.auilabs.lab1.Category;
+import jachu.pg.auilabs.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class CategoryService {
+    private final CategoryRepository categoryRepository;
+
+    @Autowired
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<Category> findAll() {
+        return categoryRepository.findAll();
+    }
+
+    public Optional<Category> findById(UUID uuid) {
+        return categoryRepository.findByUuid(uuid);
+    }
+
+    public Optional<Category> findByName(String name) {
+        return categoryRepository.findByName(name);
+    }
+
+    public Category save(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    public void deleteByUuid(UUID uuid) {
+        categoryRepository.deleteById(uuid);
+    }
+
+}
