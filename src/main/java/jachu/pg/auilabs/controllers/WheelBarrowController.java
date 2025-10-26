75a7935f6eb463cd3c47842941023253c4cbdc08
notJachu <jasiustasiu50@interia.pl>
2025-10-26 15:28:02 +0100

    WheelBarrowController get mapping

---
+++ src/main/java/jachu/pg/auilabs/controllers/WheelBarrowController.java
@@ -0,0 +1,59 @@
+package jachu.pg.auilabs.controllers;
+
+import jachu.pg.auilabs.entities.DTOs.CategoryCollectionDto;
+import jachu.pg.auilabs.entities.DTOs.WheelBarrowCollectionDto;
+import jachu.pg.auilabs.entities.DTOs.WheelBarrowReadDto;
+import jachu.pg.auilabs.entities.WheelBarrow;
+import jachu.pg.auilabs.services.CategoryService;
+import jachu.pg.auilabs.services.WheelBarrowService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/wheelbarrows")
+public class WheelBarrowController {
+
+    private final WheelBarrowService wheelBarrowService;
+
+    public WheelBarrowController(WheelBarrowService wheelBarrowService) {
+        this.wheelBarrowService = wheelBarrowService;
+    }
+
+    @GetMapping("")
+    public List<WheelBarrowCollectionDto> getWheelBarrows() {
+        List<WheelBarrow> wheelBarrows = wheelBarrowService.findAll();
+        List<WheelBarrowCollectionDto> wheelBarrowDtos = wheelBarrows.stream()
+                .map(wheelBarrow -> new WheelBarrowCollectionDto(
+                        wheelBarrow.getUuid(),
+                        wheelBarrow.getName()
+                ))
+                        .toList();
+        return wheelBarrowDtos;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<WheelBarrowReadDto> getWheelBarrowById(@PathVariable UUID id) {
+        WheelBarrow wheelBarrow = wheelBarrowService.findById(id).orElse(null);
+
+        if (wheelBarrow == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        WheelBarrowReadDto wheelBarrowDto = new WheelBarrowReadDto(
+                wheelBarrow.getUuid(),
+                wheelBarrow.getName(),
+                wheelBarrow.getPrice(),
+                wheelBarrow.getCategory() != null ? new CategoryCollectionDto(
+                        wheelBarrow.getCategory().getUuid(),
+                        wheelBarrow.getCategory().getName()
+                ) : null
+        );
+        return ResponseEntity.ok(wheelBarrowDto);
+    }
+}
