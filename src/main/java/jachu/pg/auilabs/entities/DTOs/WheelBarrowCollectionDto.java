704df02ceb4a242d2f24e5f26d54551a3a8fc5e5
notJachu <jasiustasiu50@interia.pl>
2025-10-26 15:07:58 +0100

    WheelBarrowCollectionDto implementation

--- src/main/java/jachu/pg/auilabs/entities/DTOs/WheelBarrowCollectionDto.java
+++ src/main/java/jachu/pg/auilabs/entities/DTOs/WheelBarrowCollectionDto.java
@@ -4,10 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.util.UUID;
 
-    @Data
-    @AllArgsConstructor
-    @NoArgsConstructor
-    @Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class WheelBarrowCollectionDto {
+    UUID uuid;
+    String name;
 }
