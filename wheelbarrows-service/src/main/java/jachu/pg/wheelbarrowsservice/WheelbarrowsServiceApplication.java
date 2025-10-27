2fca051f597cb52541b38707b7cbed29a67869f9
notJachu <jasiustasiu50@interia.pl>
2025-10-27 19:16:43 +0100

    wheelbarrow-service: init pt2

---
+++ wheelbarrows-service/src/main/java/jachu/pg/wheelbarrowsservice/WheelbarrowsServiceApplication.java
@@ -0,0 +1,13 @@
+package jachu.pg.wheelbarrowsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheelbarrowsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WheelbarrowsServiceApplication.class, args);
+    }
+
+}
