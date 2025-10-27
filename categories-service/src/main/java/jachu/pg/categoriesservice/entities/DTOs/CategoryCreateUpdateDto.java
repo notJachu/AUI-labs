ed950c25c0b9b16fda24dafb2ead14e44a06d940
notJachu <jasiustasiu50@interia.pl>
2025-10-27 18:54:59 +0100

    categories-service: copied DTOs

---
+++ categories-service/src/main/java/jachu/pg/categoriesservice/entities/DTOs/CategoryCreateUpdateDto.java
@@ -0,0 +1,15 @@
+package jachu.pg.categoriesservice.entities.DTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CategoryCreateUpdateDto {
+    String name;
+    int carryWeight;
+}
