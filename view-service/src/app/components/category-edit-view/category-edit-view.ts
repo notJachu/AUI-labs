29343009138c67b1221255b5c7504db695447969
notJachu <jasiustasiu50@interia.pl>
2025-11-02 21:14:24 +0100

    view-service: editing view implemented

---
+++ view-service/src/app/components/category-edit-view/category-edit-view.ts
@@ -0,0 +1,39 @@
+import {Component, inject} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {ActivatedRoute} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-category-edit-view',
+  imports: [
+    FormsModule
+  ],
+  templateUrl: './category-edit-view.html',
+  styleUrl: './category-edit-view.css',
+})
+export class CategoryEditView {
+  private http = inject(HttpClient);
+  category: any = null;
+  private route = inject(ActivatedRoute)
+
+  constructor() {
+    this.loadCategory();
+  }
+
+  loadCategory() {
+    const categoryId = this.route.snapshot.paramMap.get('id');
+    this.http.get<any>('http://localhost:8080/api/categories/' + categoryId,
+    ).subscribe(data => {
+      this.category = data;
+    });
+  }
+
+  onSubmit() {
+    const categoryId = this.route.snapshot.paramMap.get('id');
+    this.http.put<any>('http://localhost:8080/api/categories/' + categoryId, this.category).subscribe(data => {
+      this.category = data;
+      alert('Category updated successfully!');
+    });
+
+  }
+}
