b42340144bb9dac921e11e8cec47df37d3b58c53
notJachu <jasiustasiu50@interia.pl>
2025-11-02 21:13:38 +0100

    editing

--- view-service/src/app/app.routes.ts
+++ view-service/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import {CategoriesView} from './components/categories-view/categories-view';
 import {CategoryView} from './components/category-view/category-view';
+import {CategoryEditView} from './components/category-edit-view/category-edit-view';
 
 export const routes: Routes = [
   {
@@ -10,6 +11,10 @@ export const routes: Routes = [
   {
     path: 'categories/:id',
     component: CategoryView,
+  },
+  {
+    path: 'categories/edit/:id',
+    component: CategoryEditView,
   }
 ];
 
