e6bee78afd7fc8995858a653676dbc991dc4317b
notJachu <jasiustasiu50@interia.pl>
2025-11-09 18:11:23 +0100

    view-service: create category init

---
+++ view-service/src/app/components/category-create-view/category-create-view.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CategoryCreateView } from './category-create-view';
+
+describe('CategoryCreateView', () => {
+  let component: CategoryCreateView;
+  let fixture: ComponentFixture<CategoryCreateView>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CategoryCreateView]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CategoryCreateView);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ view-service/src/app/components/category-create-view/category-create-view.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-create-view',
+  imports: [],
+  templateUrl: './category-create-view.html',
+  styleUrl: './category-create-view.css',
+})
+export class CategoryCreateView {
+
+}
