d63a503fe0a2fe06a3b0d0ab297dc9946590d51d
notJachu <jasiustasiu50@interia.pl>
2025-11-09 18:25:48 +0100

    view-service: create-category-view implemented

--- view-service/src/app/app.routes.ts
+++ view-service/src/app/app.routes.ts
@@ -2,11 +2,16 @@ import { Routes } from '@angular/router';
 import {CategoriesView} from './components/categories-view/categories-view';
 import {CategoryView} from './components/category-view/category-view';
 import {CategoryEditView} from './components/category-edit-view/category-edit-view';
+import {CategoryCreateView} from './components/category-create-view/category-create-view';
 
 export const routes: Routes = [
   {
     path: 'categories',
     component: CategoriesView,
+  },
+  {
+    path: 'categories/new',
+    component: CategoryCreateView,
   },
   {
     path: 'categories/:id',
--- view-service/src/app/components/category-create-view/category-create-view.ts
+++ view-service/src/app/components/category-create-view/category-create-view.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {Router} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-category-create-view',
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './category-create-view.html',
   styleUrl: './category-create-view.css',
 })
 export class CategoryCreateView {
+  private http = inject(HttpClient);
+  private router = inject(Router)
+  category: any = {
+    name: '',
+    carryWeight: 0
+  };
 
+  onSubmit() {
+    this.http.post<any>('http://localhost:8080/api/categories', this.category).subscribe(data => {
+      this.category = data;
+      alert('Category created successfully!');
+      this.router.navigate(['/categories'])
+    });
+  }
 }
